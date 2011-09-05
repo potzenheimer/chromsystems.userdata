66b7bfe60fa3a9db3449a088927344118d38e7db
Christoph Boehner <cb@vorwaerts-werbung.de>
2011-09-05 15:00:04 +0200

    Setup the final groups - next we implement these in the importer

--- chromsystems/userdata/setuphandlers.py
+++ chromsystems/userdata/setuphandlers.py
@@ -3,8 +3,12 @@ from Products.CMFCore.utils import getToolByName
 def setupGroups(portal):
     acl_users = getToolByName(portal, 'acl_users')
     gtool = getToolByName(portal, 'portal_groups')
-    if not acl_users.searchGroups(name='Testgroup'):
-        gtool.addGroup('Testgroup', roles=['Member'])
+    if not acl_users.searchGroups(name='Wordwide'):
+        gtool.addGroup('Worldwide', roles=['Member'])
+    if not acl_users.searchGroups(name='GermanSpeakingCountries'):
+        gtool.addGroup('GermanSpeakingCountries', roles=['Member'])
+    if not acl_users.searchGroups(name='Netherlands'):
+        gtool.addGroup('Netherlands', roles=['Member'])
 
 def importVarious(context):
     """Miscellanous import steps handler"""
